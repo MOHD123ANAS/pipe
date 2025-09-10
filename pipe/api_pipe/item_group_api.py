cbcfc4bcdcd177d52a3b2b728f69e39f94942e1f
MOHD123ANAS <mohammedanas18025@gmail.com>
2025-09-10 15:57:04 +0530

    item group update

--- pipe/api_pipe/item_group_api.py
+++ pipe/api_pipe/item_group_api.py
@@ -2,18 +2,18 @@ import frappe
 
 @frappe.whitelist()
 def get_items_by_parent_groups(limit=None):
-    """Return items grouped by top-level parent item groups (excluding 'All Item Groups')."""
+    
     result = {}
-    limit = int(limit) if limit else None  # Convert limit from string to int
+    limit = int(limit) if limit else None
 
-    # Step 1: Get all parent groups under "All Item Groups"
+    
     parent_groups = frappe.get_all(
         "Item Group",
         filters={"parent_item_group": "All Item Groups"},
         fields=["name"]
     )
 
-    # Step 2: For each parent group, fetch items including itself and descendants
+    
     for pg in parent_groups:
         items_data = []
         items = frappe.get_all(
@@ -23,11 +23,11 @@ def get_items_by_parent_groups(limit=None):
                 ["custom_is_this_a_website_item", "=", 1]
             ],
             fields=["name", "item_name", "item_group", "stock_uom", "standard_rate", "gst_hsn_code", "description"],
-            limit_page_length=limit  # <-- Apply the limit here
+            limit_page_length=limit 
         )
 
         for item in items:
-            # Fetch all attachments for this item
+            
             attachments = frappe.get_all(
                 "File",
                 filters={
