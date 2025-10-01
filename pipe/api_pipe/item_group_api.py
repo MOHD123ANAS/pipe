a31105f639c6e8a43a5b51dbd9dcafe1328df290
MOHD123ANAS <mohammedanas18025@gmail.com>
2025-10-01 11:07:42 +0530

    hero-page-category-api

--- pipe/api_pipe/item_group_api.py
+++ pipe/api_pipe/item_group_api.py
@@ -1,19 +1,25 @@
 import frappe
 
+def get_item_prices(item_names, price_list="Standard Selling"):
+    prices = frappe.get_all(
+        "Item Price",
+        filters={"item_code": ["in", item_names], "price_list": price_list},
+        fields=["item_code", "price_list_rate"]
+    )
+    return {p["item_code"]: p["price_list_rate"] for p in prices}
+
 @frappe.whitelist()
 def get_items_by_parent_groups(limit=None):
     
     result = {}
     limit = int(limit) if limit else None
 
-    
     parent_groups = frappe.get_all(
         "Item Group",
         filters={"parent_item_group": "All Item Groups"},
         fields=["name"]
     )
 
-    
     for pg in parent_groups:
         items_data = []
         items = frappe.get_all(
@@ -26,8 +32,14 @@ def get_items_by_parent_groups(limit=None):
             limit_page_length=limit 
         )
 
+        
+        item_names = [item["name"] for item in items]
+        prices_map = get_item_prices(item_names, price_list="Standard Selling")
+
         for item in items:
             
+            item["standard_rate"] = prices_map.get(item["name"], 0)
+
             attachments = frappe.get_all(
                 "File",
                 filters={
